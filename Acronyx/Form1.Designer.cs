8e304860dabd123c013479c5838020febc4bfb34
fgrando <fernandozatt@gmail.com>
2021-08-23 18:52:02 -0300

    Added basic labels

--- Acronyx/Form1.Designer.cs
+++ Acronyx/Form1.Designer.cs
@@ -29,89 +29,203 @@ namespace Acronyx
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPageInput = new System.Windows.Forms.TabPage();
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.tabPageEdit = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPageResult = new System.Windows.Forms.TabPage();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.ColumnIgnore = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1Acronym = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMeaning = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabPageInput.SuspendLayout();
+            this.tabPageEdit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPageResult.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPageInput);
+            this.tabControl1.Controls.Add(this.tabPageEdit);
+            this.tabControl1.Controls.Add(this.tabPageResult);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(402, 270);
+            this.tabControl1.Size = new System.Drawing.Size(381, 223);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl1_Selecting);
             // 
-            // tabPage1
+            // tabPageInput
             // 
-            this.tabPage1.Controls.Add(this.richTextBox2);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(394, 244);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPageInput.Controls.Add(this.richTextBoxInput);
+            this.tabPageInput.Location = new System.Drawing.Point(4, 22);
+            this.tabPageInput.Name = "tabPageInput";
+            this.tabPageInput.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageInput.Size = new System.Drawing.Size(373, 197);
+            this.tabPageInput.TabIndex = 0;
+            this.tabPageInput.Text = "1. Paste text here";
+            this.tabPageInput.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // richTextBoxInput
             // 
-            this.tabPage2.Controls.Add(this.richTextBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(394, 244);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.richTextBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxInput.Location = new System.Drawing.Point(3, 3);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(367, 191);
+            this.richTextBoxInput.TabIndex = 0;
+            this.richTextBoxInput.Text = "";
+            this.richTextBoxInput.TextChanged += new System.EventHandler(this.richTextBoxInput_TextChanged);
             // 
-            // richTextBox1
+            // tabPageEdit
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(388, 238);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.tabPageEdit.Controls.Add(this.dataGridView1);
+            this.tabPageEdit.Location = new System.Drawing.Point(4, 22);
+            this.tabPageEdit.Name = "tabPageEdit";
+            this.tabPageEdit.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEdit.Size = new System.Drawing.Size(373, 197);
+            this.tabPageEdit.TabIndex = 1;
+            this.tabPageEdit.Text = "2. Edit the acronyms";
+            this.tabPageEdit.UseVisualStyleBackColor = true;
             // 
-            // richTextBox2
+            // dataGridView1
             // 
-            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(388, 238);
-            this.richTextBox2.TabIndex = 0;
-            this.richTextBox2.Text = "";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnIgnore,
+            this.Column1Acronym,
+            this.ColumnMeaning});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(367, 191);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPageResult
+            // 
+            this.tabPageResult.Controls.Add(this.richTextBoxOutput);
+            this.tabPageResult.Location = new System.Drawing.Point(4, 22);
+            this.tabPageResult.Name = "tabPageResult";
+            this.tabPageResult.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageResult.Size = new System.Drawing.Size(373, 197);
+            this.tabPageResult.TabIndex = 2;
+            this.tabPageResult.Text = "3. Copy the result table";
+            this.tabPageResult.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxOutput.Location = new System.Drawing.Point(3, 3);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.ReadOnly = true;
+            this.richTextBoxOutput.Size = new System.Drawing.Size(367, 191);
+            this.richTextBoxOutput.TabIndex = 0;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // ColumnIgnore
+            // 
+            this.ColumnIgnore.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.ColumnIgnore.Frozen = true;
+            this.ColumnIgnore.HeaderText = "Ignore";
+            this.ColumnIgnore.Name = "ColumnIgnore";
+            this.ColumnIgnore.Width = 43;
+            // 
+            // Column1Acronym
+            // 
+            this.Column1Acronym.Frozen = true;
+            this.Column1Acronym.HeaderText = "Acronym";
+            this.Column1Acronym.Name = "Column1Acronym";
+            this.Column1Acronym.ReadOnly = true;
+            // 
+            // ColumnMeaning
+            // 
+            this.ColumnMeaning.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnMeaning.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ColumnMeaning.HeaderText = "Definition";
+            this.ColumnMeaning.Name = "ColumnMeaning";
+            this.ColumnMeaning.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "&Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 270);
+            this.ClientSize = new System.Drawing.Size(381, 247);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Acronyx";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.tabPageInput.ResumeLayout(false);
+            this.tabPageEdit.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPageResult.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabPage tabPageInput;
+        private System.Windows.Forms.TabPage tabPageEdit;
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.TabPage tabPageResult;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnIgnore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1Acronym;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMeaning;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
     }
 }
 
--- Acronyx/Form1.cs
+++ Acronyx/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Acronyx
 {
@@ -14,6 +15,98 @@ namespace Acronyx
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // returns a list of unique acronyms alphabetically ordered
+        private void listAcronyms(ref List<string> list)
+        {
+            string pattern = @"[A-Z0-9]{2,}";
+
+            list.Clear();
+            //string pattern = @"\b(([A-Z][A-Z]+)";
+            Regex rgx = new Regex(pattern);
+
+            foreach (Match match in rgx.Matches(this.richTextBoxInput.Text))
+            {
+                if (!list.Contains(match.Value))
+                    list.Add(match.Value);
+            }
+
+            list.Sort();
+        }
+
+        private void renderTable(IDictionary<string, string> dic)
+        {
+            StringBuilder strTable = new StringBuilder();
+            int cellWidth = 1000;
+
+            this.richTextBoxOutput.Clear();
+
+            strTable.Append(@"{\rtf1");
+
+            foreach (KeyValuePair<string, string> acronym in dic)
+            {
+                strTable.Append(@"\trowd");
+
+                strTable.Append(@"\cellx"+cellWidth.ToString()).AppendLine(acronym.Key);
+
+                strTable.Append(@"\intbl\cell");
+
+                strTable.Append(@"\cellx" + (2*cellWidth).ToString()).AppendLine(acronym.Value);
+
+                strTable.Append(@"\intbl \cell \row");
+            }
+
+            strTable.Append(@"\pard");
+            strTable.Append(@"}");
+
+            richTextBoxOutput.Rtf = strTable.ToString();
+        }
+
+        private void process()
+        {
+            var acronyms = new List<string>();
+
+            // get all acronyms from INPUT
+            listAcronyms(ref acronyms);
+
+            // list all finding in the EDIT
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            foreach (string acronym in acronyms)
+            {
+                Console.WriteLine(acronym);
+                dataGridView1.Rows.Add(false, acronym, "XX");
+                dic.Add(acronym, "XX\nYY");
+            }
+
+            // show output in TABLE
+            renderTable(dic);
+        }
+
+        private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            TabPage current = (sender as TabControl).SelectedTab;
+
+            Console.WriteLine(current.Text);
+            process();
+        }
+
+        private void richTextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            string txt = richTextBoxInput.Text;
+            Console.WriteLine(txt);
+            richTextBoxInput.Text = txt;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBoxInput.Clear();
+            richTextBoxInput.Text = "If you enable rich-text formatting for a rich text box, users can use a variety of options to format the text that they enter in that control.AFK Away From Keyboard BBIAB - Be Back In A Bit BBL - Be Back Later BBS - Be Back Soon EG - Evil Grin For example, they can apply a different font or character style to the text inside the rich text box or even insert a table into the rich text box.BEG Big Evil Grin.BRB - Be Right Back BTW By The Way. Depending on the options that you choose, users also may be able to link to or embed pictures in the rich text box.Embedding a picture stores the picture as part of the form and is a good choice if YOUR users don't need to update the picture. Linking to a picture stores the picture outside the form and is a better choice if you think that your users will need to update the linked picture, or if you want to reduce the file size of the completed form. If users link to a picture, they must have access to the location of the picture source file. Otherwise, the picture will not appear in the rich text box.";
         }
     }
 }
