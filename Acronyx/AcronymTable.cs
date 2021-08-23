7a7cc1eb15993ff05e8b1b35f38607b313e49af7
fgrando <fernandozatt@gmail.com>
2021-08-23 19:14:37 -0300

    separating control

---
+++ Acronyx/AcronymTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Acronyx
+{
+    class AcronymTable
+    {
+        Dictionary<string, string> definitions = new Dictionary<string, string>();
+        Dictionary<string, string> current = new Dictionary<string, string>();
+
+        public void build(List<string> acronyms)
+        {
+            foreach (string acronym in acronyms)
+            {
+                current.Add(acronym, "XX\nYY");
+            }
+        }
+        public IDictionary<string, string> toDictionary()
+        {
+            return current;
+        }
+
+        public bool saveDic(string filepath)
+        {
+            Console.WriteLine("saving dic to " + filepath);
+            System.IO.File.WriteAllText(filepath, "test");
+            return true;
+        }
+        public bool loadDic(string filepath)
+        {
+            Console.WriteLine("loading dic from " + filepath);
+            string text = System.IO.File.ReadAllText(filepath);
+            return true;
+        }
+    }
+}
--- Acronyx/Form1.cs
+++ Acronyx/Form1.cs
@@ -12,9 +12,11 @@ namespace Acronyx
 {
     public partial class Form1 : Form
     {
+        AcronymTable acronymData = new AcronymTable();
         public Form1()
         {
             InitializeComponent();
+            richTextBoxInput.Text = "If you enable rich-text formatting for a rich text box, users can use a variety of options to format the text that they enter in that control.AFK Away From Keyboard BBIAB - Be Back In A Bit BBL - Be Back Later BBS - Be Back Soon EG - Evil Grin For example, they can apply a different font or character style to the text inside the rich text box or even insert a table into the rich text box.BEG Big Evil Grin.BRB - Be Right Back BTW By The Way. Depending on the options that you choose, users also may be able to link to or embed pictures in the rich text box.Embedding a picture stores the picture as part of the form and is a good choice if YOUR users don't need to update the picture. Linking to a picture stores the picture outside the form and is a better choice if you think that your users will need to update the linked picture, or if you want to reduce the file size of the completed form. If users link to a picture, they must have access to the location of the picture source file. Otherwise, the picture will not appear in the rich text box.";
         }
 
         // returns a list of unique acronyms alphabetically ordered
@@ -32,7 +34,8 @@ namespace Acronyx
                     list.Add(match.Value);
             }
 
-            list.Sort();
+            list = list.Distinct().ToList(); // unique
+            list.Sort(); // sorted
         }
 
         private void renderTable(IDictionary<string, string> dic)
@@ -71,16 +74,10 @@ namespace Acronyx
             listAcronyms(ref acronyms);
 
             // list all finding in the EDIT
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-            foreach (string acronym in acronyms)
-            {
-                Console.WriteLine(acronym);
-                dataGridView1.Rows.Add(false, acronym, "XX");
-                dic.Add(acronym, "XX\nYY");
-            }
+            acronymData.build(acronyms);
 
             // show output in TABLE
-            renderTable(dic);
+            renderTable(acronymData.toDictionary());
         }
 
         private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
@@ -100,13 +97,30 @@ namespace Acronyx
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text file|*.txt";
+            saveFileDialog1.Title = "Save dictionary File";
+            saveFileDialog1.ShowDialog();
 
+            if (saveFileDialog1.FileName != "")
+            {
+                acronymData.saveDic(saveFileDialog1.FileName);
+            }
         }
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            richTextBoxInput.Clear();
-            richTextBoxInput.Text = "If you enable rich-text formatting for a rich text box, users can use a variety of options to format the text that they enter in that control.AFK Away From Keyboard BBIAB - Be Back In A Bit BBL - Be Back Later BBS - Be Back Soon EG - Evil Grin For example, they can apply a different font or character style to the text inside the rich text box or even insert a table into the rich text box.BEG Big Evil Grin.BRB - Be Right Back BTW By The Way. Depending on the options that you choose, users also may be able to link to or embed pictures in the rich text box.Embedding a picture stores the picture as part of the form and is a good choice if YOUR users don't need to update the picture. Linking to a picture stores the picture outside the form and is a better choice if you think that your users will need to update the linked picture, or if you want to reduce the file size of the completed form. If users link to a picture, they must have access to the location of the picture source file. Otherwise, the picture will not appear in the rich text box.";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "dictionary-1.txt",
+                Filter = "Text file|*.txt",
+                Title = "Load a dictionary file"
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                acronymData.loadDic(openFileDialog1.FileName);
+            }
         }
     }
 }
